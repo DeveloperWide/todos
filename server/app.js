f4c39a1a4a6df9652a332c865439ebd652ddd026
Mahesh rana <maheshisdelta@gmail.com>
2025-05-26 20:41:12 +0530

    first-commit

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const connectDb = require("./config/db");
+
+connectDb()
+
+// Routes
+let taskRoutes = require("./routes/taskRoute");
+
+
+const corsOptions = {
+  origin: 'http://localhost:5173/',
+  optionsSuccessStatus: 200
+}
+
+// middlewares
+app.use(cors(corsOptions));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Task Routes
+app.use("/api/tasks/", taskRoutes)
+
+app.listen(PORT , () => {
+    console.log(`Server is listing on PORT ${PORT}`)
+})
